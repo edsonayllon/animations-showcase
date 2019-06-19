7b5af389bb9cd012d148adbf2bf58801cef381a6
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-06-19 18:19:11 +0000

    animation working

--- client/screens/AnimatedTiming.js
+++ client/screens/AnimatedTiming.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -10,10 +10,40 @@ import {
 import { Link } from '../navigation';
 
 export default function AnimationTiming() {
-  const [spinValue, setSpinValue] = useState(new Animated.Value(0));
+  const spinValue = new Animated.Value(0);
+  const spining = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  })
+
+  const spin = () => {
+    spinValue.setValue(0);
+    Animated.timing(
+      spinValue,
+      {
+        toValue: 1,
+        duration: 5000,
+        easing: Easing.linear
+      }
+    ).start(() => spin());
+  }
+
+  useEffect(() => {
+    spin();
+  }, [])
+
   return (
     <View>
       <Link to="/"><Text>Back</Text></Link>
+      <Animated.Image
+        style={{
+          width: 227,
+          height: 200,
+          marginTop: 40,
+          transform: [{rotate: spining}]
+        }}
+        source={{uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png'}}
+      />
     </View>
   )
 }
