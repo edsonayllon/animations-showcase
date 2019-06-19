7b10692aa0b32a025f0be9cd2d56bc231f525381
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-06-19 17:44:19 +0000

    added first hook

--- client/screens/AnimatedTiming.js
+++ client/screens/AnimatedTiming.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Image,
+  Easing
+} from 'react-native';
 import { Link } from '../navigation';
 
 export default function AnimationTiming() {
+  const [spinValue, setSpinValue] = useState(new Animated.Value(0));
   return (
     <View>
       <Link to="/"><Text>Back</Text></Link>
